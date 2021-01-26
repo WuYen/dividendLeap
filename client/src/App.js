5ad80d3e5c4873607405f054f0fd7c0534b80497
YenLap <s810643@gmail.com>
2021-01-27 00:11:44 +0800

    add react router in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
 import MainContent from "./component/MainContent";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <MainContent />
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/page2">page2</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/page2">
+            <div>page2</div>
+          </Route>
+          <Route path="/">
+            <MainContent />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
