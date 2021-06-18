b6b98a2d55d3bd8f8fc8a5b536fb3d375742cbe5
Yen <s810643@gmail.com>
2021-06-18 11:39:58 +0800

    Revise server and connectDB for stability

--- utility/connectDB.js
+++ utility/connectDB.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
 
 function connectMongo(URI, callBack) {
-  mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
   mongoose.connection.on("connected", () => {
     console.log("Mongoose is connected!!!!");
-    callBack();
+  });
+
+  return mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
 }
 
