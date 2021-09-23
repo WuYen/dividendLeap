fc1aed36dfd88e078ed83466bae07d23ed3e268a
Ryan.Chang <ryan.chang@Xuenn.com>
2021-09-23 17:38:35 +0800

    Add mail config

--- utility/config.js
+++ utility/config.js
@@ -6,6 +6,8 @@ const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
 const DIVIDENDINFO_URL = process.env.DIVIDENDINFO_URL;
 const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
+const MAIL_ACCOUNT = process.env.MAIL_ACCOUNT;
+const MAIL_PASSWORD = process.env.MAIL_PASSWORD;
 
 module.exports = {
   NODE_ENV,
@@ -13,4 +15,6 @@ module.exports = {
   PORT,
   DIVIDENDINFO_URL,
   TOKEN_SECRET,
+  MAIL_ACCOUNT,
+  MAIL_PASSWORD,
 };
