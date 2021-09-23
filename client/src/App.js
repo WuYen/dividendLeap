f84538cc8b8fcba73234351230aa9345e6a93c6b
Ryan.Chang <ryan.chang@Xuenn.com>
2021-09-23 11:55:51 +0800

    Fix React error "You should not use <Link> outside a <Router>"

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,8 @@ export default function App() {
   return (
     <Provider store={store}>
       <ChakraProvider>
-        <Header />
         <Router>
+          <Header />
           <Routes />
         </Router>
       </ChakraProvider>
