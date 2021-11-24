f241cc2295fd3fd0fb8a2ad06da990bf2181208d
Yen <s810643@gmail.com>
2021-11-25 00:14:43 +0800

    Fix production config error missing /api

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,11 +1,9 @@
 const useLocalServer = true;
 
 export const dataAPI =
-  process.env.NODE_ENV === "development"
-    ? useLocalServer
-      ? "http://localhost:8080/api"
-      : "https://fierce-eyrie-95732.herokuapp.com/api"
-    : "";
+  process.env.NODE_ENV === "development" && useLocalServer
+    ? "http://localhost:8080/api"
+    : "https://fierce-eyrie-95732.herokuapp.com/api";
 
 export const socketAPI =
   process.env.NODE_ENV === "development" ? "http://localhost:8080" : "https://fierce-eyrie-95732.herokuapp.com";
