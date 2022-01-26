c1f19d1bcfcc01576cd4d27f2d8ee3b5cab482f4
Yen <s810643@gmail.com>
2022-01-27 00:45:09 +0800

    Integrate fugleApi to stocksocket

--- client/src/components/StockSocket.jsx
+++ client/src/components/StockSocket.jsx
@@ -1,25 +1,57 @@
 import React, { useCallback, useEffect, useState, useRef } from "react";
 import { Box, Flex, Grid, Button } from "@chakra-ui/react";
+import { useMyStocks } from "../hooks/useMyStock";
+const manage = Manager();
 
 export default function StockSocket(props) {
   const keywordRef = useRef();
-  const [stockNo, setStockNo] = useState();
+  const [stockNo, setStockNo] = useState([]);
+  const [myStock] = useMyStocks();
+  let typeData = myStock.filter((x) => x.type == "我的清單");
   const style = { padding: "5px", border: "1px solid black", margin: "2px" };
+  useEffect(() => {
+    manage.startRunning();
+    return () => {
+      manage.stopRunning();
+    };
+  }, []);
   return (
     <div>
-      <input style={style} ref={keywordRef}></input>
+      {/* <input style={style} ref={keywordRef}></input>
       <button
         style={style}
         onClick={() => {
-          setStockNo(keywordRef.current.value);
+          setStockNo((x) => [...x, keywordRef.current.value]);
         }}
       >
         Watch
-      </button>
-      {stockNo && <StockBox stockNo={stockNo} />}
+      </button> */}
+      {typeData.map(({ stockNo }) => (
+        <StockBox key={stockNo} stockNo={stockNo} />
+      ))}
     </div>
   );
 }
+
+// export default function StockSocket(props) {
+//   const keywordRef = useRef();
+//   const [stockNo, setStockNo] = useState();
+//   const style = { padding: "5px", border: "1px solid black", margin: "2px" };
+//   return (
+//     <div>
+//       <input style={style} ref={keywordRef}></input>
+//       <button
+//         style={style}
+//         onClick={() => {
+//           setStockNo(keywordRef.current.value);
+//         }}
+//       >
+//         Watch
+//       </button>
+//       {stockNo && <StockBox stockNo={stockNo} />}
+//     </div>
+//   );
+// }
 
 const token = "";
 
@@ -28,10 +60,16 @@ function StockBox(props) {
   const [price, setPrice] = useState(null);
   const [meta, setMeta] = useState(null);
   const serialRef = useRef(0);
-
+  const isMount = useRef(false);
   useEffect(() => {
-    fetch(`https://api.fugle.tw/realtime/v0.3/intraday/meta?symbolId=${stockNo}&apiToken=${token}`)
-      .then((res) => res.json())
+    isMount.current = true;
+    return () => {
+      isMount.current = false;
+    };
+  }, []);
+  useEffect(() => {
+    manage
+      .addJob(`https://api.fugle.tw/realtime/v0.3/intraday/meta?symbolId=${stockNo}&apiToken=${token}`)
       .then((data) => {
         console.log("meta", data);
         // "meta":{
@@ -53,36 +91,57 @@ function StockBox(props) {
         //   "abnormal":"正常",
         //   "isUnusuallyRecommended":false
         //   }
-        setMeta(data.data.meta);
+        isMount.current && setMeta(data.data.meta);
       });
   }, [stockNo]);
 
   useEffect(() => {
-    var ws = new WebSocket(`wss://api.fugle.tw/realtime/v0.3/intraday/quote?symbolId=${stockNo}&apiToken=${token}`);
-    ws.onopen = function () {
-      console.log("open connection");
-    };
+    manage
+      .addJob(`https://api.fugle.tw/realtime/v0.3/intraday/quote?symbolId=${stockNo}&apiToken=${token}`)
+      .then((data) => {
+        console.log("quote", stockNo, data);
+        if (data.data.quote.trade.serial > serialRef.current && isMount.current) setPrice(data.data.quote.trade.price);
+        serialRef.current = data.data.quote.trade.serial;
+      });
+    var id = setInterval(() => {
+      manage
+        .addJob(`https://api.fugle.tw/realtime/v0.3/intraday/quote?symbolId=${stockNo}&apiToken=${token}`)
+        .then((data) => {
+          console.log("quote", stockNo, data);
+          if (data.data.quote.trade.serial > serialRef.current && isMount.current)
+            setPrice(data.data.quote.trade.price);
+          serialRef.current = data.data.quote.trade.serial;
+        });
+    }, 10000);
 
-    ws.onclose = function () {
-      console.log("disconnected");
-    };
+    // var ws = new WebSocket(`wss://api.fugle.tw/realtime/v0.3/intraday/quote?symbolId=${stockNo}&apiToken=${token}`);
+    // ws.onopen = function () {
+    //   console.log("open connection");
+    // };
 
-    ws.onmessage = function (message) {
-      var data = JSON.parse(message.data);
-      console.log(`onmessage`, data);
-      // "trade":{
-      //   "at":"2022-01-21T13:30:00.000+08:00",
-      //   "bid":44.5,
-      //   "ask":44.55,
-      //   "price":44.5,
-      //   "volume":847,
-      //   "serial":8126473
-      //   },
-      if (data.data.quote.trade.serial > serialRef.current) setPrice(data.data.quote.trade.price);
-      serialRef.current = data.data.quote.trade.serial;
-    };
+    // ws.onclose = function () {
+    //   console.log("disconnected");
+    // };
+
+    // ws.onmessage = function (message) {
+    //   var data = JSON.parse(message.data);
+    //   console.log(`onmessage`, data);
+    //   // "trade":{
+    //   //   "at":"2022-01-21T13:30:00.000+08:00",
+    //   //   "bid":44.5,
+    //   //   "ask":44.55,
+    //   //   "price":44.5,
+    //   //   "volume":847,
+    //   //   "serial":8126473
+    //   //   },
+    //   if (data.data.quote.trade.serial > serialRef.current) setPrice(data.data.quote.trade.price);
+    //   serialRef.current = data.data.quote.trade.serial;
+    // };
+    // return () => {
+    //   ws.close();
+    // };
     return () => {
-      ws.close();
+      clearInterval(id);
     };
   }, [stockNo]);
 
@@ -125,4 +184,75 @@ function FlexRowTwoCol(props) {
       </Box>
     </Flex>
   );
+}
+
+function Manager() {
+  let jobQueue = [];
+  let counter = 0;
+  let stopped = true;
+
+  const batchSize = 10;
+
+  async function DoJob() {
+    if (stopped) {
+      return;
+    }
+    let tasks = [];
+    let idx = 0;
+    let jobLength = Math.min(batchSize, jobQueue.length);
+    let t1 = +new Date();
+    console.log(`run job [start] length:${jobLength}, total:${jobQueue.length}`);
+    while (idx < jobLength) {
+      var { url, res } = jobQueue[idx];
+      tasks.push(
+        fetch(url)
+          .then((response) => response.json())
+          .then((data) => res(data))
+      );
+      idx++;
+    }
+    jobQueue.splice(0, jobLength);
+    await Promise.all([
+      ...tasks,
+      new Promise((res, rej) => {
+        setTimeout(() => {
+          res();
+        }, batchSize * 1000);
+      }),
+    ]);
+    let t2 = +new Date();
+    console.log(`run job [finish] remain:${jobQueue.length}`, t2 - t1);
+    counter = 0;
+    !stopped && DoJob();
+  }
+
+  function addJob(url, callBack = null) {
+    //第一次進queue的要能插隊 => 第一次 queue裡面沒有stockNo的就是第一次
+    console.log("add job", `counter:${counter}`);
+
+    let job =
+      counter < batchSize
+        ? fetch(url).then((res) => res.json())
+        : new Promise((res, rej) => {
+            jobQueue.push({ url, res });
+          });
+
+    counter++;
+
+    return callBack ? job.then(callBack) : job;
+  }
+
+  return {
+    startRunning: () => {
+      if (stopped) {
+        stopped = false;
+        DoJob();
+      }
+    },
+    stopRunning: () => {
+      stopped = true;
+    },
+    addJob,
+    removeJob: () => {},
+  };
 }
--- client/src/utils/fugleApi.js
+++ client/src/utils/fugleApi.js
@@ -1,62 +1,57 @@
 function Manager() {
-  let fugleQueue = [];
+  let jobQueue = [];
   let counter = 0;
   let stopped = true;
 
-  //fugleQueue.shift()
-  //fugleQueue.push()
   const batchSize = 10;
 
   async function DoJob() {
     if (stopped) {
       return;
     }
-    let task = [];
+    let tasks = [];
     let idx = 0;
-    let jobLength = Math.min(batchSize, fugleQueue.length);
-    console.log(`run job length:${jobLength}`);
+    let jobLength = Math.min(batchSize, jobQueue.length);
+    let t1 = +new Date();
+    console.log(`run job [start] length:${jobLength}, total:${jobQueue.length}`);
     while (idx < jobLength) {
-      var { stockNo, res } = fugleQueue[idx];
-      console.log("do fetch", idx);
-      let job = fetch("https://jsonplaceholder.typicode.com/todos/1")
-        .then((response) => response.json())
-        .then((data) => {
-          res(data);
-        });
-
+      var { stockNo, res } = jobQueue[idx];
+      tasks.push(
+        fetch("https://jsonplaceholder.typicode.com/todos/1")
+          .then((response) => response.json())
+          .then((data) => res(data))
+      );
       idx++;
-      task.push(job);
     }
-    fugleQueue.splice(0, jobLength);
-    let finish = Promise.all([
-      ...task,
+    jobQueue.splice(0, jobLength);
+    await Promise.all([
+      ...tasks,
       new Promise((res, rej) => {
         setTimeout(() => {
           res();
         }, batchSize * 1000);
       }),
     ]);
-    console.log(`run job finish remain:${fugleQueue.length}`);
-    await finish;
+    let t2 = +new Date();
+    console.log(`run job [finish] remain:${jobQueue.length}`, t2 - t1);
     counter = 0;
     !stopped && DoJob();
   }
 
-  function addJob(stockNo, callBack) {
+  function addJob(stockNo, callBack = null) {
     //第一次進queue的要能插隊 => 第一次 queue裡面沒有stockNo的就是第一次
     console.log("add job", `counter:${counter}`, stockNo);
-    if (counter < batchSize) {
-      counter++;
-      let job = fetch("https://jsonplaceholder.typicode.com/todos/1").then((res) => res.json());
-      return callBack ? job.then(callBack) : job;
-    } else {
-      counter++;
-      let job = new Promise((res, rej) => {
-        fugleQueue.push({ stockNo, res });
-      });
 
-      return callBack ? job.then(callBack) : job;
-    }
+    let job =
+      counter < batchSize
+        ? fetch("https://jsonplaceholder.typicode.com/todos/1").then((res) => res.json())
+        : new Promise((res, rej) => {
+            jobQueue.push({ stockNo, res });
+          });
+
+    counter++;
+
+    return callBack ? job.then(callBack) : job;
   }
 
   return {
